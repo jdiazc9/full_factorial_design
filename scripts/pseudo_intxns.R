af01e2bc91a43fbb68d0b45a27df424c3cc9826d
Juan Díaz-Colunga <juan.diazcolunga@gmail.com>
2024-04-29 12:15:23 +0200

    insets of Fig. 5E and slight cleanup

--- scripts/pseudo_intxns.R
+++ scripts/pseudo_intxns.R
@@ -266,6 +266,10 @@ ggsave(myplot,
        limitsize = F)
 
 
+# interactions and functional effects within this consortium
+dffmax <- df[df$community %in% focal_comms, ]
+
+
 
 
 ### DIVERSITY-FUNCTION
@@ -734,394 +738,83 @@ ggsave(filename = '../plots/dfe.pdf',
        limitsize = F)
 
 
-if(FALSE) {
-  
-  
-  ### BEYOND-PAIRWISE DOMINANCE MODEL
-  
-  singles <- data[data$nspecies == 1, ]
-  
-  tst <- sapply(data$community[data$nspecies > 1],
-                FUN = function(comm) {
-                  
-                  singles_in_comm <- sapply(which(strsplit(comm, split = '')[[1]] == 1),
-                                            FUN = function(sp) {
-                                              out <- rep(0, 8)
-                                              out[sp] <- 1
-                                              out <- paste(out, collapse = '')
-                                              return(out)
-                                            })
-                  singles_in_comm <- singles[singles$community %in% singles_in_comm, ]
-                  
-                  comm_spect <- data[data$community == comm, c('wavelength', 'absorbance', 'sd')]
-                  comm_spect <- merge(singles_in_comm, comm_spect,
-                                      by = 'wavelength',
-                                      all = T,
-                                      suffixes = c('_singles', '_coculture'))
-                  comm_spect$diff <- comm_spect$absorbance_coculture - comm_spect$absorbance_singles
-                  comm_spect <- aggregate(diff ~ community,
-                                          data = comm_spect,
-                                          FUN = function(x) sum(x^2))
-                  dom_monoc <- comm_spect$community[which.min(comm_spect$diff)]
-                  
-                  return(dom_monoc)
-                  
-                })
-  
-  
-  
-  
-  
- 
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  ### COMPLETE EXCLUSION MODEL
-  # species are ranked from most to least competitive
-  # the spectrum of a consortium c is modeled as the spectrum of the most competitive species in c
-  
-  data <- data_full
-  monoc <- data[data$nspecies == 1 & data$wavelength == focal_wavelength, ]
-  consortia <- data[data$nspecies > 1 & data$wavelength == focal_wavelength, ]
-  unique_consortia <- unique(consortia$community)
-  
-  set.seed(0)
-  ranks <- sample(1:8)
-  
-  exclusionModel <- function(target_consortia, ranks = 1:8) {
-    
-    do.call(rbind,
-            lapply(target_consortia,
-                   FUN = function(target_consortium) {
-                     
-                     sp_in_target <- which(strsplit(target_consortium, split = '')[[1]] == '1')
-                     most_competitive_sp <- sp_in_target[which.min(ranks[sp_in_target])]
-                     
-                     pred <- rep(0, 8)
-                     pred[most_competitive_sp] <- 1
-                     pred <- paste(pred, collapse = '')
-                     
-                     predicted_spect <- monoc[monoc$community == pred, c('community', 'wavelength', 'absorbance', 'sd')]
-                     colnames(predicted_spect)[3:4] <- paste(colnames(predicted_spect)[3:4], '_predicted', sep = '')
-                     predicted_spect$community <- target_consortium
-                     
-                     return(predicted_spect)
-                     
-                   }))
-    
-  }
-  
-  # find optimal rank
-  ranks_to_test <- permn(8)
-  st <- Sys.time()
-  ranks_R2 <- unlist(pblapply(ranks_to_test,
-                              FUN = function(rank_i) {
-                                
-                                predictedSpect <- exclusionModel(unique_consortia, ranks = rank_i)
-                                predictedSpect <- merge(predictedSpect, consortia, by = c('community', 'wavelength'), all = T)
-                                
-                                R2 <- summary(lm(absorbance ~ absorbance_predicted,
-                                                 data = predictedSpect))$r.squared
-                                
-                                return(R2)
-                                
-                              }))
-  end <- Sys.time()
-  end - st
-  
-  
-  
-  
-  
-  ### PLOTS
-  
-  # plot layout
-  design <- matrix(NA, nrow = landscape_size + 1,
-                   ncol = max(c(choose(landscape_size, ceiling(landscape_size/2)), choose(landscape_size, floor(landscape_size/2)))))
-  panel_index <- 1
-  for (i in 0:landscape_size) {
-    n_panels <- choose(landscape_size, i)
-    design[i + 1, 1:n_panels] <- panel_index:(panel_index + n_panels - 1)
-    panel_index <- panel_index + n_panels
-  }
-  
-  # sort communities by number of species & add aux. variables
-  data$community_id <- binary_to_integer(data$community)
-  data <- data[order(data$nspecies, data$community_id, decreasing = F), ]
-  data$community <- factor(data$community, levels = unique(data$community))
-  data$spec <- 'co-culture'
-  
-  
-  ### PLOT
-  
-  # base plot
-  myplot <-
-    ggplot(data,
-           aes(x = wavelength, y = absorbance,
-               ymin = absorbance - sd, ymax = absorbance + sd,
-               group = spec, color = spec, fill = spec)) +
-    geom_blank() +
-    # geom_line(linewidth = 0.25,
-    #           color = 'black') +
-    scale_x_continuous(breaks = c(500, 700),
-                       name = 'Wavelength (nm)') +
-    scale_y_continuous(breaks = pretty_breaks(n = 2),
-                       name = 'Absorbance') +
-    facet_manual(vars(community),
-                 design = design,
-                 widths = 1,
-                 heights = 0.3) +
-    theme_bw() + 
-    theme(aspect.ratio = 0.3,
-          panel.grid = element_blank(),
-          strip.background = element_blank(),
-          legend.position = 'none',
-          panel.border = element_blank()) +
-    annotate("segment", x=-Inf, xend=Inf, y=-Inf, yend=-Inf, linewidth=0.5) +
-    annotate("segment", x=-Inf, xend=-Inf, y=-Inf, yend=Inf, linewidth=0.5)
-  
-  
-  
-  # plot monocultures
-  
-  monocs_sum_all <- data.frame(wavelength = numeric(0),
-                               community = character(0),
-                               nspecies = numeric(0),
-                               absorbance = numeric(0),
-                               spec = character(0))
-  
-  for (comm in unique(data$community)) {
-    
-    monocs <- which(strsplit(as.character(comm), split = '')[[1]] == '1')
-    if(length(monocs) > 1) {
-      
-      monocs <- sapply(1:length(monocs),
-                       FUN = function(i) {
-                         monoc_id <- rep('0', landscape_size)
-                         monoc_id[monocs[i]] <- '1'
-                         monoc_id <- paste(monoc_id, collapse = '')
-                         return(monoc_id)
-                       })
-      
-      monocs_spect <- do.call(rbind,
-                              lapply(monocs,
-                                     FUN = function(m) {
-                                       df <- data[data$community == m, ]
-                                       df$spec <- m
-                                       return(df)
-                                     }))
-      monocs_spect$community <- comm
-      monocs_spect$community <- factor(monocs_spect$community,
-                                       levels = levels(data$community))
-      
-      myplot <- myplot +
-        geom_line(data = monocs_spect,
-                  linewidth = 0.25,
-                  linetype = 'dashed')
-      
-      # sum of monoc. spectra
-      if(length(monocs) > 1) {
-        
-        monocs_sum <- aggregate(absorbance ~ wavelength + community + nspecies,
-                                data = monocs_spect,
-                                FUN = sum)
-        monocs_sum$sd <- NA
-        monocs_sum$spec <- 'sum'
-        
-        myplot <- myplot +
-          geom_line(data = monocs_sum,
-                    linewidth = 0.5,
-                    linetype = 'dashed')
-        
-        monocs_sum_all <- rbind(monocs_sum_all,
-                                monocs_sum)
-        
-      }
-      
-    }
-    
-  }
-  
-  # plot consortia spectra
-  for (comm in unique(data$community)) {
-    if(sum(strsplit(as.character(comm), split = '')[[1]] == '1') != 1) {
-      
-      myplot <- myplot +
-        geom_ribbon(data = data[data$community == comm, ],
-                    color = NA,
-                    alpha = 0.2) +
-        geom_line(data = data[data$community == comm, ],
-                  linewidth = 0.5)
-      
-    }
-  }
-  
-  # plot monocultures
-  singles <- data[data$nspecies == 1, ]
-  singles$spec <- singles$community
-  
-  myplot <- myplot +
-    geom_ribbon(data = singles,
-                color = NA,
-                alpha = 0.2) +
-    geom_line(data = singles,
-              linewidth = 0.5)
-  
-  # adjust colors for nice aesthetics
-  mycolors <- c('#E76D57', '#ACD152', '#7A9DD2', '#F7AFBE', '#E5C91F', '#A88BC0', '#5CA985', '#BBAD75')
-  
-  myplot <- myplot +
-    scale_color_manual(name = 'Species',
-                       values = c(mycolors, 'black', 'black')) +
-    scale_fill_manual(name = 'Species',
-                      values = c(mycolors, 'black', 'black'))
-  
-  ggsave(myplot,
-         filename = paste('../plots/pseudo_spectra.pdf', sep = ''),
-         width = 1500,
-         height = 200,
-         units = 'mm',
-         limitsize = F)
-  
-  
-  myplot <- myplot +
-    scale_y_log10(breaks = c(0.1, 1, 10),
-                  name = 'Absorbance')
-  
-  ggsave(myplot,
-         filename = paste('../plots/pseudo_spectra_log.pdf', sep = ''),
-         width = 1500,
-         height = 200,
-         units = 'mm',
-         limitsize = F)
-  
-  
-  # small plot for main fig
-  data <- data[order(data$community_id), ]
-  data$community <- factor(data$community,
-                           levels = unique(data$community))
-  myplot_main <- 
-    ggplot(data,
-           aes(x = wavelength, y = absorbance, ymin = absorbance - sd, ymax = absorbance + sd)) +
-    geom_ribbon(color = NA,
-                fill = 'black',
-                alpha = 0.2) +
-    geom_line(color = 'black') +
-    # facet_wrap(~ community,
-    #            nrow = 8,
-    #            dir = 'v') +
-    facet_wrap2(~ community,
-                nrow = 8,
-                dir = 'v',
-                axes = 'all',
-                remove_labels = 'all') +
-    # scale_y_continuous(name = 'Absorbance (A.U.)',
-    #                    breaks = pretty_breaks(n = 2)) +
-    scale_y_log10(name = 'Absorbance (A.U.)',
-                  breaks = seq(0.1, 1, length.out = 10)) +
-    scale_x_continuous(name = 'Wavelength (nm)',
-                       breaks = c(500, 700)) +
-    theme_bw() + 
-    theme(aspect.ratio = 1,
-          panel.grid = element_blank(),
-          strip.background = element_blank(),
-          legend.position = 'none',
-          panel.border = element_blank(),
-          axis.line.x = element_line(),
-          axis.line.y = element_line(),
-          axis.text = element_text(size = 13),
-          axis.title = element_text(size = 16),
-          #strip.text = element_text(size = 8),
-          axis.ticks.length = unit(0.25, "mm"),
-          #axis.ticks = element_blank(),
-          strip.text = element_blank())
-  # annotate("segment", x=-Inf, xend=Inf, y=0.09, yend=0.09, linewidth=0.5) +
-  # annotate("segment", x=-Inf, xend=-Inf, y=0.09, yend=Inf, linewidth=0.5)
-  
-  ggsave(myplot_main,
-         filename = paste('../plots/pseudo_spectra_main_log_ticks.pdf', sep = ''),
-         width = 300,
-         height = 150,
-         units = 'mm',
-         limitsize = F)
-  
-  
-  ### QUANTIFY INTERACTIONS
-  
-  # get interaction coefficients at all orders
-  focal_wavelength <- 600
-  data <- data[data$wavelength == focal_wavelength, c('community', 'absorbance')]
-  df <- do.call(rbind,
-                lapply(1:nrow(data),
-                       FUN = function(i) as.data.frame(t(strsplit(as.character(data$community[i]), split = '')[[1]]))))
-  df$y <- data$absorbance
-  colnames(df) <- c(paste('x', 1:landscape_size), 'y')
-  
-  # which base?
-  base <- 'taylor' # one of 'taylor' or 'fourier'
-  if (base == 'fourier') {
-    for (i in 1:landscape_size) df[df[, i] == 0, i] <- -1
-  }
-  
-  # model <- lm(y ~ . + .*. + .*.*. + .*.*.*. + .*.*.*.*. + .*.*.*.*.*. + .*.*.*.*.*.*. + .*.*.*.*.*.*.*.,
-  #             data = df)
-  model <- lm(y ~ .*.*.*.*.*.*.*.,
-              data = df)
-  coef <- model$coefficients
-  names(coef) <- gsub('`1', '', names(coef))
-  names(coef) <- gsub('`x', '', names(coef))
-  names(coef) <- gsub(' ', '', names(coef))
-  names(coef) <- gsub(':', '', names(coef))
-  names(coef)[1] <- ''
-  
-  coef_df <- data.frame(coef_id = names(coef),
-                        coef_order = nchar(names(coef)),
-                        value = as.numeric(coef))
-  ggplot(coef_df, aes(x = coef_order, y = value, group = coef_order)) +
-    geom_abline(slope = 0,
-                intercept = 0,
-                color = 'gray') +
-    geom_boxplot(data = coef_df[coef_df$coef_order > 0 & coef_df$coef_order < 8, ],
-                 fill = 'white',
-                 outlier.shape = NA) +
-    geom_jitter(width = 0.15,
-                alpha = 0.25) +
-    scale_x_continuous(name = 'Order',
-                       breaks = 0:8) +
-    scale_y_continuous(name = expression(paste('Coefficient value, ', italic(beta), ' (A.U.)', sep = ''))) +
-    theme_bw() + 
-    theme(aspect.ratio = 0.6,
-          panel.grid = element_blank(),
-          strip.background = element_blank(),
-          legend.position = 'none',
-          panel.border = element_blank(),
-          axis.text = element_text(size = 13),
-          axis.title = element_text(size = 16)) +
-    annotate("segment", x=-Inf, xend=Inf, y=-Inf, yend=-Inf, linewidth=0.5) +
-    annotate("segment", x=-Inf, xend=-Inf, y=-Inf, yend=Inf, linewidth=0.5)
-  
-  ggsave(filename = paste('../plots/pseudo_coef_', base, '.pdf', sep = ''),
-         width = 100,
-         height = 100,
-         units = 'mm',
-         limitsize = F)
-  
-  model_y <- predict(model, df[, 1:landscape_size])
-  
-  
-}
+
+# functional effects & interactions within best-consortium sub-landscape
+dfe <- dfe[dfe$background %in% focal_comms & dfe$species %in% paste('Species', c(1, 3, 4)), ]
+dfi <- dfi[dfi$background %in% focal_comms & dfi$species_i %in% paste('Species', c(1, 3, 4)) & dfi$species_j %in% paste('Species', c(1, 3, 4)), ]
+
+ggplot(dfe,
+       aes(x = gsub('Species ', '', species), y = delta_i, color = species)) +
+  geom_hline(yintercept = 0, color = 'gray') +
+  geom_point() +
+  geom_boxplot(outlier.shape = NA,
+               width = 0.5,
+               alpha = 0.25,
+               fill = NA) +
+  scale_y_continuous(name = expression(paste(Delta*italic(Abs), ' (A.U.)', sep = '')),
+                     breaks = pretty_breaks(n = 3),
+                     limits = range(c(dfe$delta_i, dfi$eps_ij))) +
+  scale_x_discrete(name = 'Species') +
+  scale_color_manual(values = c('#E76D57',
+                                '#7A9DD2',
+                                '#F7AFBE')) +
+  theme_bw() +
+  theme(aspect.ratio = 1.6,
+        panel.grid = element_blank(),
+        strip.background = element_blank(),
+        strip.text = element_text(size = 13,
+                                  angle = 0),
+        strip.text.y.left = element_text(angle = 0),
+        strip.text.x = element_text(angle = 90),
+        axis.text = element_text(size = 13),
+        axis.title = element_text(size = 16),
+        axis.ticks.x = element_blank(),
+        panel.border = element_blank(),
+        legend.position = 'none') +
+  annotate("segment", x=-Inf, xend=Inf, y=-Inf, yend=-Inf, linewidth=0.5) +
+  annotate("segment", x=-Inf, xend=-Inf, y=-Inf, yend=Inf, linewidth=0.5)
+
+ggsave(filename = '../plots/dfe_sublandscape.pdf',
+       width = 50,
+       height = 50,
+       units = 'mm',
+       limitsize = F)
 
 
+
+ggplot(dfi,
+       aes(x = paste(gsub('Species ', '', species_i), gsub('Species ', '', species_j), sep = '-'),
+           y = eps_ij)) +
+  geom_hline(yintercept = 0, color = 'gray') +
+  geom_point() +
+  geom_boxplot(outlier.shape = NA,
+               width = 0.25,
+               alpha = 0.25) +
+  scale_y_continuous(name = expression(paste('Functional interaction, ', italic(epsilon), ' (A.U.)', sep = '')),
+                     breaks = pretty_breaks(n = 3),
+                     limits = range(c(dfe$delta_i, dfi$eps_ij))) +
+  scale_x_discrete(name = 'Species pair') +
+  theme_bw() +
+  theme(aspect.ratio = 2,
+        panel.grid = element_blank(),
+        strip.background = element_blank(),
+        strip.text = element_text(size = 13,
+                                  angle = 0),
+        strip.text.y.left = element_text(angle = 0),
+        strip.text.x = element_text(angle = 90),
+        axis.text = element_text(size = 13),
+        axis.title = element_text(size = 16),
+        axis.ticks.x = element_blank(),
+        panel.border = element_blank(),
+        legend.position = 'none') +
+  annotate("segment", x=-Inf, xend=Inf, y=-Inf, yend=-Inf, linewidth=0.5) +
+  annotate("segment", x=-Inf, xend=-Inf, y=-Inf, yend=Inf, linewidth=0.5)
+
+ggsave(filename = '../plots/dfi_sublandscape.pdf',
+       width = 50,
+       height = 100,
+       units = 'mm',
+       limitsize = F)
 
 
 
